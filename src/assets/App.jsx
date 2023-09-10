affadb24298344afa57ae439a1d74512e9aaa125
unknown <nikhilsharrma1998@gmail.com>
2023-09-11 04:10:56 +0530

    Add Hashrouter

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -1,6 +1,6 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import PageA from "./components/PageA";
@@ -9,12 +9,12 @@ import PageB from "./components/PageB";
 function App() {
 	return (
 		<Provider store={store}>
-			<BrowserRouter basename='/'>
+			<HashRouter basename='/'>
 				<Routes>
 					<Route path='/' Component={PageA} />
 					<Route path='/pageB' Component={PageB} />
 				</Routes>
-			</BrowserRouter>
+			</HashRouter>
 		</Provider>
 	);
 }
